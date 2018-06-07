bad26ee59fa1db247a947e157a73d6190dd9e122
HashRoot97 <roothash97@gmail.com>
2018-06-07 11:43:28 +0530

    Bug fixes to make csv

--- utils/make_csv.py
+++ utils/make_csv.py
@@ -1,15 +1,16 @@
 import os
 from xml.etree import ElementTree as ET
-
+import csv
 
 
 def get_dict(ann_dir):
 
 	dict_main = {
-		'filename' : [],
+		'file_path' : [],
 		'classes' : [],
-		'bbox' : []
+		'roi' : []
 	}
+	img_source_path = './../Dataset/Train/VOCdevkit/VOC2007/JPEGImages/'
 
 	for file in os.listdir(ann_dir):
 		file = ann_dir + file
@@ -28,16 +29,28 @@ def get_dict(ann_dir):
 		                             for child3 in child2:
 		                                     a.append(child3.text)
 		             bbox.append(a)
+		     if child1.tag == 'filename':
+		     	img_name = child1.text
 
 		dict_main['classes'].append(classes)
-		dict_main['bbox'].append(bbox)
-		dict_main['filename'].append(file)
+		dict_main['roi'].append(bbox)
+		dict_main['file_path'].append(img_source_path + img_name)
+	os.system('touch ./../data.csv')
+	csv_file = './../data.csv'
+
+	with open(csv_file, 'w') as f:
+
+		for i in range(len(dict_main['file_path'])):
+			a = '"{}"'.format(str(dict_main['file_path'][i]))
+			b = '"{}"'.format(str(dict_main['roi'][i]))
+			c = '"{}"'.format(str(dict_main['classes'][i]))
+
+			line = a + ',' + b + ',' + c + '\n'
+			f.write(line)
+
 	return dict_main
 
 
 ann_dir = './../Dataset/Train/VOCdevkit/VOC2007/Annotations/'
 dict_main = get_dict(ann_dir)
-
-print(len(dict_main['classes']))
-print(len(dict_main['bbox']))
-print(len(dict_main['filename']))
+print(dict_main.keys())
