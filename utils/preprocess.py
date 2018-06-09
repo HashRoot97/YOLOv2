968b71614886ce988dc8ebd67d3334304a6f80e7
HashRoot97 <roothash97@gmail.com>
2018-06-09 21:18:59 +0530

    Bug fixes and plotting

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -20,8 +20,9 @@ def pre_process(dict_main):
 
 
 def resize_img(image_loc, roi, i):
+    img_size = (416, 416)
     im = np.array(Image.open(image_loc), dtype=np.uint8)
-    img = cv2.resize(im, (200, 200))
+    img = cv2.resize(im, img_size)
     new_image_loc = './../ResizedJPEGImages/' + str(i) + '.jpg'
     cv2.imwrite(new_image_loc, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
     new_roi = []
@@ -30,8 +31,8 @@ def resize_img(image_loc, roi, i):
         x_ = im.shape[1]
         y_ = im.shape[0]
         x_min_, y_min_, x_max_, y_max_ = (roi[i][0], roi[i][1], roi[i][2], roi[i][3])
-        x_scale = (200 / y_)
-        y_scale = (200 / x_)
+        x_scale = (416 / y_)
+        y_scale = (416 / x_)
         x_min = int(np.round(x_min_ * y_scale))
         y_min = int(np.round(y_min_ * x_scale))
         x_max = int(np.round(x_max_ * y_scale))
